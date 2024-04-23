66c7c2a1664a0385838f68af3fcea0688f4e8d48
Liliia.Lyshchuk <liliia.lyshchuk@gmail.com>
2024-04-23 12:09:58 +0300

    Create Welcome & Pages Controllers, add routes to controllers

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,22 @@
+class PagesController < ApplicationController
+    def about 
+    end
+
+    def gallery 
+    end
+
+    def contact 
+    end
+
+    def vacancy 
+    end
+
+    def cart 
+    end
+
+    def menu 
+    end
+
+    def account 
+    end
+end
