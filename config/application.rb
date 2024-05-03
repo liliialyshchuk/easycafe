347d5f1cb5a26290159eb7ff6df1ba491b909de8
Liliia.Lyshchuk <liliia.lyshchuk@gmail.com>
2024-05-03 22:55:38 +0300

    change precompile

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,7 @@ module Easycafe
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.i18n.default_locale = :en
+
+    config.assets.initialize_on_precompile = false
   end
 end
